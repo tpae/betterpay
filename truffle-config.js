907608e89a7812a9fbd3018201f92119f4cb68e4
Terence Pae <terencepae@gmail.com>
2019-11-23 14:33:07 -0800

    updated minor deployment settings

--- truffle-config.js
+++ truffle-config.js
@@ -1,17 +1,16 @@
 require('dotenv').config();
-const mnemonic = process.env.MNEMONIC;
+const mnemonic = process.env.MNEMONIC || 'frog hidden corn close nominee expire frame expose sort tonight village divorce';
 const path = require("path");
 const HDWalletProvider = require("truffle-hdwallet-provider");
 // Create your own key for Production environments (https://infura.io/)
 const INFURA_ID = process.env.INFURA_ID || 'e34c32bc829b495b8c286a346992214e';
 
-
-const configNetwork = (network, networkId, path = "m/44'/60'/0'/0/", gas = 4465030, gasPrice = 1e10) => ({
+const configNetwork = (network, networkId, path = "m/44'/60'/0'/0/", gas = 6000000, gasPrice = 10000000000) => ({
   provider: () => new HDWalletProvider(
     mnemonic, `https://${network}.infura.io/v3/${INFURA_ID}`, 
         0, 1, true, path
     ),
-  networkId,
+  network_id: networkId,
   gas,
   gasPrice,
 });
