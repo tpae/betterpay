d29c8959d53bcebd1444dd0da06e917692eb17c6
Svetly <svetly@gmail.com>
2020-01-26 19:26:19 -0800

    update

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,17 +4,11 @@ import { Flex, Text, Button, Box } from 'rimble-ui';
 export default function Home(props) {
   return (
     <Flex flexDirection="column" height="100%" justifyContent="center" alignItems="center">
-      <h1>Borderless, peer-to-peer digital payments w/ escrow</h1>
+      <h1>Borderless, peer-to-peer digital payments with escrow</h1>
       <Box height={25} />
       <Flex flexDirection="row">
         <Flex flexDirection="column">
-          <Button size="large" px={50} onClick={props.onBuyer}>Buy</Button>
-          <Text textAlign="center" italic>Pay</Text>
-        </Flex>
-        <Box width={50} />
-        <Flex flexDirection="column">
-          <Button size="large" px={50} onClick={props.onSeller}>Sell</Button>
-          <Text textAlign="center" italic>Request Payment</Text>
+          <Button size="large" px={50} onClick={props.onSeller}>New Payment</Button>
         </Flex>
       </Flex>
     </Flex>
